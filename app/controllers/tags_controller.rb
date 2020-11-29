b8acdcb5fb44b5942c44caf1b61c871d6f0a65de
Janos Rusiczki <janos.rusiczki@softvision.ro>
2020-11-29 12:50:49 +0200

    Fix tag page photo ordering

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,6 +3,6 @@ class TagsController < ApplicationController
 
   def show
     @tag = ActsAsTaggableOn::Tag.friendly.find(params[:id])
-    @pagy, @photos = pagy(Photo.tagged_with(@tag))
+    @pagy, @photos = pagy(Photo.tagged_with(@tag).order(date_taken: :desc))
   end
 end
