f0ce97d46e5cd0c85300334d05d2d59bfb263839
János Rusiczki <janos.rusiczki@gmail.com>
2022-10-20 22:27:07 +0300

    Update structure.sql

--- db/structure.sql
+++ db/structure.sql
@@ -20,7 +20,7 @@ CREATE EXTENSION IF NOT EXISTS unaccent WITH SCHEMA public;
 -- Name: EXTENSION unaccent; Type: COMMENT; Schema: -; Owner: -
 --
 
-COMMENT ON EXTENSION unaccent IS 'text search dictionary that removes accents';
+-- COMMENT ON EXTENSION unaccent IS 'text search dictionary that removes accents';
 
 
 --
