50e7ebf157a28d2e4c622c7d9dd1232502582e4f
Janos Rusiczki <janos.rusiczki@gmail.com>
2022-11-22 16:43:29 +0200

    Redirect to root after signing out

--- app/javascript/users/sign-out.vue
+++ app/javascript/users/sign-out.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    Signing out
+    Signing out...
   </div>
 </template>
 
@@ -8,6 +8,9 @@
   import gql from 'graphql-tag'
   import { useMutation } from '@vue/apollo-composable'
   import { useUserStore } from '../stores/user'
+  import { useRouter } from 'vue-router'
+
+  const router = useRouter()
 
   const { mutate, onDone, onError } = useMutation(
     gql`
@@ -21,6 +24,7 @@
     const userStore = useUserStore()
     userStore.signedIn = false
     userStore.email = null
+    router.push({ name: 'root' })
   })
 
   onError((error) => {
