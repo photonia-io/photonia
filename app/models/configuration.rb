8fe6a01bb66d76d87c429f77fc1f4675cca9b801
Janos Rusiczki <janos.rusiczki@gmail.com>
2023-03-20 00:04:03 +0200

    Fix forgotten comma

--- app/models/configuration.rb
+++ app/models/configuration.rb
@@ -4,7 +4,7 @@
 class Configuration
   attr_accessor :root_path, :graphql_url, :photos_path, :albums_path, :tags_path, :users_sign_in_path, :users_sign_out_path, :users_settings_path
 
-  def initialize(root_path:, graphql_url:, photos_path:, albums_path:, tags_path:, , users_sign_in_path:, users_sign_out_path:, users_settings_path:)
+  def initialize(root_path:, graphql_url:, photos_path:, albums_path:, tags_path:, users_sign_in_path:, users_sign_out_path:, users_settings_path:)
     @root_path   = root_path
     @graphql_url = graphql_url
     @photos_path = photos_path
