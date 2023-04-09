c59497896d2ede7285b119a6a8d77e7904e70c93
János Rusiczki <janos.rusiczki@gmail.com>
2023-04-09 23:11:51 +0300

    Remove cached GraphQL loader

--- app/controllers/concerns/homepage_loader.rb
+++ app/controllers/concerns/homepage_loader.rb
@@ -13,20 +13,20 @@ module HomepageLoader
     @random_photo = Photo.order(Arel.sql('RANDOM()')).first
     @most_used_tags = ActsAsTaggableOn::Tag.photonia_most_used(limit: 60)
 
-    cached_gql
+    # cached_gql
   end
 
   private
 
-  def cached_gql
-    @gql_cached_query = GraphqlQueryCollection::COLLECTION[:homepage_index]
-    @gql_cached_result = PhotoniaSchema.execute(
-      @gql_cached_query,
-      root_value: {
-        latest_photo: @latest_photo,
-        random_photo: @random_photo,
-        most_used_tags: @most_used_tags
-      }
-    ).to_json
-  end
+  # def cached_gql
+  #   @gql_cached_query = GraphqlQueryCollection::COLLECTION[:homepage_index]
+  #   @gql_cached_result = PhotoniaSchema.execute(
+  #     @gql_cached_query,
+  #     root_value: {
+  #       latest_photo: @latest_photo,
+  #       random_photo: @random_photo,
+  #       most_used_tags: @most_used_tags
+  #     }
+  #   ).to_json
+  # end
 end
