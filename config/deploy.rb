696bbcdb6395b6757ff00e2f943e68418d9303a8
Janos Rusiczki <janos.rusiczki@gmail.com>
2023-11-06 15:06:03 +0200

    Fix capistrano version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # config valid for current version and patch releases of Capistrano
-lock '~> 3.17.2'
+lock '~> 3.18.0'
 
 set :application, 'photonia'
 set :repo_url, 'git@github.com:photonia-io/photonia.git'
