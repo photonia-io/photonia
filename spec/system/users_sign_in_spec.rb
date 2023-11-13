8e1a7c96f0a6cfb162ede76b20d8cb301edc19ae
Janos Rusiczki <janos.rusiczki@gmail.com>
2023-11-13 11:07:48 +0200

    Sign in system spec rubocopped

--- spec/system/users_sign_in_spec.rb
+++ spec/system/users_sign_in_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 describe 'Sign In' do
@@ -5,29 +7,28 @@ describe 'Sign In' do
   let(:password) { 'Password123!' }
 
   before do
-    create(:user, email: email, password: password)
+    create(:user, email:, password:)
   end
 
   context 'with valid email and password' do
-    it 'signs in the user' do
+    it 'signs in the user and redirects to user settings page' do
       sign_in_with(email:, password:)
+      expect(page).to have_current_path(users_settings_path)
       expect(page).to have_field('email', with: email, disabled: true)
     end
   end
 
   context 'with invalid email' do
-    it 'does not sign in the user' do
+    it 'displays an error message' do
       sign_in_with(email: 'another.user@domain.com', password:)
       expect(page).to have_content('There was an error signing you in. Please try again.')
     end
   end
 
   context 'with invalid password' do
-    it 'does not sign in the user' do
+    it 'displays an error message' do
       sign_in_with(email:, password: 'wrongpassword')
       expect(page).to have_content('There was an error signing you in. Please try again.')
     end
   end
 end
-
-
