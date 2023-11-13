8e3d5daac257241ac71abb6d199259392c887b88
Janos Rusiczki <janos.rusiczki@gmail.com>
2023-11-13 16:16:46 +0200

    Show labels on photo hero

--- app/javascript/photos/display-hero.vue
+++ app/javascript/photos/display-hero.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="hero is-dark mb-3">
+    <div id="label-list" v-if="showLabels">
+      <span class="label-list-title">Labels</span>
+      <LabelListItem
+        v-for="label in photo.labels"
+        @highlight-label="$emit('highlightLabel', $event)"
+        @un-highlight-label="$emit('unHighlightLabel', $event)"
+        :label="label"
+        :hoverable="true"
+        :key="label.id"
+      />
+    </div>
     <div class="hero-body pt-4 pb-4" style="text-align: center">
       <div id="image-wrapper">
         <router-link
@@ -42,7 +53,10 @@
 </template>
 
 <script setup>
+import { computed } from "vue";
 import DisplayLabel from "./display-label.vue";
+import LabelListItem from "./label-list-item.vue";
+import { useApplicationStore } from "@/stores/application";
 
 const props = defineProps({
   photo: {
@@ -58,6 +72,23 @@ const props = defineProps({
     required: false,
     default: false,
   },
+  loading: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
+});
+
+const emit = defineEmits(["highlightLabel", "unHighlightLabel"]);
+
+const applicationStore = useApplicationStore();
+
+const showLabels = computed(() => {
+  return (
+    applicationStore.showLabelsOnHero &&
+    !props.loading &&
+    props.photo.labels?.length > 0
+  );
 });
 </script>
 
@@ -88,15 +119,43 @@ const props = defineProps({
   }
 }
 
-#image-wrapper > .labels {
-}
-
 .overlay {
   position: absolute;
   bottom: 0;
   left: 0;
   width: 100%;
   background: rgba(0, 0, 0, 0.5);
+}
+
+.hero {
+  position: relative;
+}
+
+#label-list {
+  position: absolute;
+  top: 0;
+  right: 0;
+  z-index: 100;
+  background: rgba(0, 0, 0, 0.3);
+  margin: 0.5rem 0.5rem 0 0;
+  padding: 0.3em;
+  width: auto;
+  display: flex;
+  flex-direction: column;
+  border-radius: 5px;
+  overflow: scroll;
+  max-height: calc(100% - 15px);
+}
+
+.label-list-title {
+  font-size: 0.8rem;
+  margin: 0 0.2rem 0.2rem;
+  font-weight: bold;
+}
+
+#label-list .tag {
+  text-align: left;
+  margin: 0.2rem;
 }
 
 // #image-wrapper > .overlay {
