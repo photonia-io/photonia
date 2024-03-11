892c033046cd16698cc8ed663f8c62b18025efe0
Janos Rusiczki <janos.rusiczki@gmail.com>
2024-03-11 21:55:09 +0200

    Fix Vue CJS warning / Fix yarn install warnings

--- app/javascript/test/pagination.test.js
+++ app/javascript/test/pagination.test.js
@@ -3,37 +3,48 @@ import { mount, RouterLinkStub } from "@vue/test-utils";
 
 test("Pagination renders the correct elements", () => {
   expect(Pagination).toBeTruthy();
+
   const wrapper = mount(Pagination, {
     props: {
       metadata: {
         currentPage: 2,
-        totalPages: 10
+        totalPages: 10,
       },
-      routeName: "photos-index"
+      routeName: "photos-index",
     },
     global: {
       stubs: {
-        RouterLink: RouterLinkStub
-      }
-    }
-  })
-  expect(wrapper.find("nav.pagination").exists()).toBe(true)
-  expect(wrapper.findAllComponents(RouterLinkStub).length).toBe(8)
-  // expect(wrapper.findAllComponents(RouterLinkStub)[0].classes()).toContain("disabled")
+        RouterLink: RouterLinkStub,
+      },
+    },
+  });
 
-  // check if the previous button leads to page 1
-  expect(wrapper.findAllComponents(RouterLinkStub)[0].props().to).toEqual({ name: "photos-index" })
+  expect(wrapper.find("nav.pagination").exists()).toBe(true);
+  expect(wrapper.findAllComponents(RouterLinkStub).length).toBe(8);
 
-  // check if the next button leads to page 3
-  expect(wrapper.findAllComponents(RouterLinkStub)[1].props().to).toEqual({ name: "photos-index", query: { page: 3 } })
+  // check if the "< Prev" button leads to page 1
+  expect(wrapper.findAllComponents(RouterLinkStub)[0].props().to).toEqual({
+    name: "photos-index",
+  });
+
+  // check if the "Next >" button leads to page 3
+  expect(wrapper.findAllComponents(RouterLinkStub)[1].props().to).toEqual({
+    name: "photos-index",
+    query: { page: 3 },
+  });
 
   // check if the first page in the list is 1
-  expect(wrapper.findAllComponents(RouterLinkStub)[2].props().to).toEqual({ name: "photos-index" })
+  expect(wrapper.findAllComponents(RouterLinkStub)[2].props().to).toEqual({
+    name: "photos-index",
+  });
 
   // check if the last page in the list is 10
-  expect(wrapper.findAllComponents(RouterLinkStub)[7].props().to).toEqual({ name: "photos-index", query: { page: 10 } })
+  expect(wrapper.findAllComponents(RouterLinkStub)[7].props().to).toEqual({
+    name: "photos-index",
+    query: { page: 10 },
+  });
 
   // console.log(wrapper.findAllComponents(RouterLinkStub)[0].classes())
   // console.log(wrapper.findAllComponents(RouterLinkStub)[0].props())
   // console.log(wrapper.findAll("nav.pagination > *")[2].classes())
-})
+});
