11a55965180a2b391b7835c5ae8c8e9b46e9bf9d
Janos Rusiczki <janos.rusiczki@gmail.com>
2024-03-19 13:47:13 +0200

    Do bundle analysis with Codecov

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,20 @@
-import { defineConfig } from 'vite'
-import RubyPlugin from 'vite-plugin-ruby'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import RubyPlugin from "vite-plugin-ruby";
+import vue from "@vitejs/plugin-vue";
+import { codecovVitePlugin } from "@codecov/vite-plugin";
 
 export default defineConfig({
   plugins: [
     RubyPlugin(),
-    vue()
+    vue(),
+    codecovVitePlugin({
+      enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+      bundleName: "photonia-io/photonia",
+      uploadToken: process.env.CODECOV_TOKEN,
+    }),
   ],
   test: {
     globals: true,
-    environment: 'happy-dom'
-  }
-})
+    environment: "happy-dom",
+  },
+});
