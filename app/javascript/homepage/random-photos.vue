66c965c5604e43b5ef6561c7ee773c79e8100210
Janos Rusiczki <janos.rusiczki@gmail.com>
2024-07-29 11:39:25 +0300

    .is-variable is not needed anymore

--- app/javascript/homepage/random-photos.vue
+++ app/javascript/homepage/random-photos.vue
@@ -1,7 +1,7 @@
 <template>
   <h2 class="title is-3 mb-2">Random Photos</h2>
   <hr class="mt-0 mb-3" />
-  <div class="columns is-variable is-multiline">
+  <div class="columns is-multiline">
     <div v-for="photo in photos" class="column is-half">
       <router-link :to="{ name: 'photos-show', params: { id: photo.id } }">
         <img
--- app/javascript/photos/show.vue
+++ app/javascript/photos/show.vue
@@ -160,7 +160,7 @@
                       {{ album.title }}
                     </router-link>
                   </h4>
-                  <div class="columns is-variable is-1 is-mobile">
+                  <div class="columns is-1 is-mobile">
                     <div class="column is-half">
                       <router-link
                         v-if="album.previousPhotoInAlbum"
