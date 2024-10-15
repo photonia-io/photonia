5df9bad39a4548ae75df66eeb3d64d44db209f70
Janos Rusiczki <janos.rusiczki@gmail.com>
2024-10-15 15:24:28 +0300

    Static pages specs

---
+++ spec/requests/pages_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Static Pages' do
+  describe 'GET /about' do
+    it 'returns http success' do
+      get '/about'
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  # describe 'GET /contact' do
+  #   it 'returns http success' do
+  #     get '/contact'
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+
+  describe 'GET /privacy-policy' do
+    it 'returns http success' do
+      get '/privacy-policy'
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET /terms-of-service' do
+    it 'returns http success' do
+      get '/terms-of-service'
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
