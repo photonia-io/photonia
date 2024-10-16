6fdf9405cb44bba7abc2dcbb39a60dfd5fb2ca62
Janos Rusiczki <janos.rusiczki@gmail.com>
2024-10-16 16:29:09 +0300

    Annotate FlickrUser spec

--- spec/models/flickr_user_spec.rb
+++ spec/models/flickr_user_spec.rb
@@ -1,5 +1,29 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: flickr_users
+#
+#  id                    :bigint           not null, primary key
+#  description           :text
+#  iconfarm              :string
+#  iconserver            :string
+#  is_deleted            :boolean
+#  location              :string
+#  nsid                  :string
+#  photos_count          :integer
+#  photos_firstdate      :integer
+#  photos_firstdatetaken :string
+#  photosurl             :string
+#  profileurl            :string
+#  realname              :string
+#  timezone_label        :string
+#  timezone_offset       :string
+#  username              :string
+#  created_at            :datetime         not null
+#  updated_at            :datetime         not null
+#  timezone_id           :string
+#
 require 'rails_helper'
 
 RSpec.describe FlickrUser do
