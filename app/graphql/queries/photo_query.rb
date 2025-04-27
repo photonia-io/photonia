4e951bca984fa00b177ebed693dae92db77ded21
Janos Rusiczki <janos.rusiczki@gmail.com>
2025-04-27 11:34:27 +0300

    Refactor PhotoQuery to utilize lookahead for conditional includes

--- app/graphql/queries/photo_query.rb
+++ app/graphql/queries/photo_query.rb
@@ -6,15 +6,19 @@ module Queries
     type Types::PhotoType, null: false
     description 'Find a photo by ID or fetch the latest photo'
 
+    extras [:lookahead]
+
     argument :fetch_type, String, 'Type of fetch operation (e.g. "by_id", "latest")', default_value: 'by_id', required: false
     argument :id, ID, 'ID of the photo', required: false, default_value: nil
 
-    def resolve(fetch_type:, id:)
-      photo = Photo.includes(:albums, :albums_photos, comments: %i[flickr_user versions])
+    def resolve(lookahead:, fetch_type:, id:)
+      photo_query = Photo
+      photo_query = photo_query.includes(:albums, :albums_photos) if lookahead.selects?(:albums)
+      photo_query = photo_query.includes(comments: %i[flickr_user versions]) if lookahead.selects?(:comments)
       photo = if fetch_type == 'latest'
-                photo.order(posted_at: :desc).first
+                photo_query.order(posted_at: :desc).first
               else
-                photo.friendly.find(id)
+                photo_query.friendly.find(id)
               end
       record_impression(photo)
       photo
