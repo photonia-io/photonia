f036aff32416e37a012ad86f86f62653a575474b
János Rusiczki <janos.rusiczki@gmail.com>
2025-07-21 09:10:58 +0300

    feat: add remove-tag Vue component for tag deletion

---
+++ app/javascript/tags/remove-tag.vue
@@ -0,0 +1,22 @@
+<template>
+  <a class="tag is-delete is-danger" title="Delete tag" @click="deleteTag"></a>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  tag: {
+    type: Object,
+    required: true,
+  },
+  photoId: {
+    type: String,
+    required: true,
+  },
+});
+
+const deleteTag = () => {
+  // Implement tag deletion logic here
+};
+</script>
