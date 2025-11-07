024cf1ecce05d43cd6c7a7e6251b4e0d9da11c87
Janos Rusiczki <janos.rusiczki@gmail.com>
2025-11-07 10:10:38 +0200

    Add comment to migration for default_license column in users table

--- db/migrate/20251107000001_add_default_license_to_users.rb
+++ db/migrate/20251107000001_add_default_license_to_users.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Migration to add default_license column to users table
 class AddDefaultLicenseToUsers < ActiveRecord::Migration[7.2]
   def change
     add_column :users, :default_license, :string, default: nil
